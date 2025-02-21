11d1c6134e47d396a69c83702804ee143fe8c4a7
daniel <danielmunicio@berkeley.edu>
2025-02-21 13:07:42 -0800

    Added Project 2

---
+++ project2/build/proj2_pkg/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/cc/ee106b/sp25/class/ee106b-abh/106b-sp25-labs-starter/project2/src/proj2_pkg
+# Build directory: /home/cc/ee106b/sp25/class/ee106b-abh/106b-sp25-labs-starter/project2/build/proj2_pkg
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ project2/src/proj2_pkg/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/cc/ee106b/sp25/class/ee106b-abh/106b-sp25-labs-starter/project2/src/proj2_pkg/src
+# Build directory: /home/cc/ee106b/sp25/class/ee106b-abh/106b-sp25-labs-starter/project2/src/proj2_pkg/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
